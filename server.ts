c6ea893e44060e2117251fb83d6f486cb8d11098
Hendra Juli <deulizealand@gmail.com>
2020-06-06 23:38:03 +0800

    initial  Backend Service Flutter project

---
+++ server.ts
@@ -0,0 +1,10 @@
+import { Application } from "https://deno.land/x/oak/mod.ts";
+import router from "./routes.ts";
+const port = 8000;
+
+const app = new Application();
+app.use(router.routes());
+app.use(router.allowedMethods());
+console.log("server Running on Port ${port}");
+
+await app.listen({ port });
