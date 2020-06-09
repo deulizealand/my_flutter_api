e93e422f03a5c9d441ab153500d6871eb88b713a
Hendra Juli <deulizealand@gmail.com>
2020-06-09 23:56:25 +0800

    add .env file support

--- database/database.ts
+++ database/database.ts
@@ -1,9 +1,10 @@
 import { Client } from "https://deno.land/x/mysql/mod.ts";
+import "https://deno.land/x/dotenv/load.ts";
 const client = await new Client().connect({
-  hostname: "127.0.0.1",
-  username: "root",
-  db: "flutter_api",
-  password: "1507",
+  hostname: Deno.env.get("hostname"),
+  username: Deno.env.get("username"),
+  db: Deno.env.get("db"),
+  password: Deno.env.get("password"),
 });
 
 export default client;
